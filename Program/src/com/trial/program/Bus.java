1ee9860c2d84c9ac63db3250d852d05855a0c906
welcome <welcome@Endiran>
2022-02-24 17:19:36 +0530

    Program

---
+++ Program/src/com/trial/program/Bus.java
@@ -0,0 +1,16 @@
+package com.trial.program;
+
+public class Bus {
+	int noofseats = 56;
+	int nooftyres = 8;
+	String Busname = "TN";
+	
+	public void green() {
+		System.out.println("Bus is green colour");
+	}
+	
+	public void property() {
+		System.out.println("This bus is government property");
+	}
+	
+}
---
+++ Program/src/com/trial/program/Bustester.java
@@ -0,0 +1,17 @@
+package com.trial.program;
+
+public class Bustester {
+	public static void main(String[] args) {
+		Bus TN= new Bus();
+		
+		System.out.println(TN.Busname);
+		System.out.println(TN.noofseats);
+		System.out.println(TN.nooftyres);
+		
+		TN.green();
+		TN.property();
+		
+	}
+	
+
+}
