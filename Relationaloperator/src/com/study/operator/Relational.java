c25fa0858e7439bedff03ff33abfa0f79f7de62b
welcome <welcome@Endiran>
2022-02-24 17:20:23 +0530

    Relational operator

---
+++ Relationaloperator/src/com/study/operator/Relational.java
@@ -0,0 +1,16 @@
+package com.study.operator;
+
+public class Relational {
+	public static void main(String[] args) {
+		int x=593;
+		int y=690;
+		
+		System.out.println(x==y);
+		System.out.println(x>=y);
+		System.out.println(x<=y);
+		System.out.println(x!=0);
+		System.out.println(x>10);
+		System.out.println(y<0);
+	}
+
+}
