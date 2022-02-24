36134623ae580c2cf353889fc04d97ca911d490c
welcome <welcome@Endiran>
2022-02-24 17:22:45 +0530

    Switch

---
+++ Switch/src/com/trial/switc/Switch.java
@@ -0,0 +1,41 @@
+package com.trial.switc;
+
+public class Switch {
+	public static void main(String[] args) {
+		int a=8;
+		
+		switch(a){
+			case 0:	
+				System.out.println("a is zero");
+			break;
+			case 1:
+				System.out.println("a is one");
+			break;
+			case 2:
+				System.out.println("a is two");
+			break;
+			case 3:
+				System.out.println("a is three");
+			break;
+			case 4:
+				System.out.println("a is four");
+			break;
+			case 5:
+				System.out.println("a is five");
+			break;
+			case 6:
+				System.out.println("a is six");
+			break;
+			case 7:
+				System.out.println("a is sevenm");
+			break;
+			case 8:
+				System.out.println("a is eight");
+			break;
+			case 9:
+				System.out.println("a is nine");
+				
+			}
+	}
+
+}
---
+++ Switch/src/com/trial/switc/Switch2.java
@@ -0,0 +1,19 @@
+package com.trial.switc;
+
+public class Switch2 {
+	public static void main(String[] args) {
+		String student = "Endiran";
+		
+	switch(student) {
+	case "Dinesh":
+		System.out.println(student + " is from Bangalore");
+		break;
+	case "Endiran":
+		System.out.println(student + " is from Chennai");
+		break;
+	case "Raja":
+		System.out.println(student + " is from Delhi");
+		}
+	}
+
+}
