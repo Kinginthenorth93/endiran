31ddb331672c60c4ac8cc3c2dd60253a0de5e024
welcome <welcome@Endiran>
2022-02-24 17:15:45 +0530

    FFirst

---
+++ First/src/First.java
@@ -0,0 +1,16 @@
+
+public class First {
+	public static void main(String[] args) {
+		int[] a = new int[3];
+		
+		a[0] = 10;
+		a[1] = 20;
+		a[2] = 30;
+		
+		for (int z=0;z<3;z=z+10) {
+			
+		System.out.println(a[z]);
+		
+		}
+	}
+}
