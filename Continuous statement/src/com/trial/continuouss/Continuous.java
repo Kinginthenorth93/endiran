78eb794a4398aedd85f3c9891583f94bc1db97a6
welcome <welcome@Endiran>
2022-02-24 17:13:53 +0530

    Continue statement

---
+++ Continuous statement/src/com/trial/continuouss/Continuous.java
@@ -0,0 +1,13 @@
+package com.trial.continuouss;
+
+public class Continuous {
+	public static void main(String[] args) {
+		for(int i=0; i<=20; i++) {
+			if(i%2==0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
