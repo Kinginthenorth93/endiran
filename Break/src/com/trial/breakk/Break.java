11667cc605a54eefff1879fd3b07c27ecba99bf1
welcome <welcome@Endiran>
2022-02-24 17:10:43 +0530

    Break statement

---
+++ Break/src/com/trial/breakk/Break.java
@@ -0,0 +1,15 @@
+package com.trial.breakk;
+
+public class Break {
+	public static void main(String[] args) {
+
+		for(int i=1; i<=100; i++) {
+			if(i==99){
+				break;
+			}
+			System.out.println(i);
+			
+		}
+	}
+
+}
---
+++ Break/src/com/trial/breakk/break2.java
@@ -0,0 +1,13 @@
+package com.trial.breakk;
+
+public class break2 {
+	public static void main(String[] args) {
+		for(int i=100; i>=1; i--) {
+			if(i==50) {
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
