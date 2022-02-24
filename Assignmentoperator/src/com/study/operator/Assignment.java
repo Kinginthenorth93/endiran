3a69b2c8d21ec5f54ee41ce3383c213b7de5fd3d
welcome <welcome@Endiran>
2022-02-24 17:03:28 +0530

    Assigment Operator

---
+++ Assignmentoperator/src/com/study/operator/Assignment.java
@@ -0,0 +1,17 @@
+package com.study.operator;
+
+public class Assignment {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+	
+		
+		System.out.println(c);
+		
+				
+
+	}
+
+
+}
