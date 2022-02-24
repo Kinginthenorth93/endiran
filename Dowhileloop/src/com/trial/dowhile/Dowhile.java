63c631dca3c2d9eb879cafe25cbfc195f5f9420e
welcome <welcome@Endiran>
2022-02-24 17:14:49 +0530

    Dowhile loop

---
+++ Dowhileloop/src/com/trial/dowhile/Dowhile.java
@@ -0,0 +1,15 @@
+package com.trial.dowhile;
+
+public class Dowhile {
+	public static void main(String[] args) {
+		int abcd=1;
+		do {
+			System.out.println(abcd);
+			abcd++;
+		}
+		while(abcd<=25);
+		   
+	}
+	
+
+}
---
+++ Dowhileloop/src/com/trial/dowhile/Dowhile2.java
@@ -0,0 +1,14 @@
+package com.trial.dowhile;
+
+public class Dowhile2 {
+public static void main(String[] args) {
+	int xyz = 60;
+	do {
+		System.out.println(xyz);
+		xyz--;
+	   }
+	while(xyz>=1);
+	
+}
+
+}
