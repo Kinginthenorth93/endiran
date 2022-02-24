18ad256d63088dcdc11a6de25e3c9492133e6375
welcome <welcome@Endiran>
2022-02-24 17:12:08 +0530

    First Constructor

---
+++ Constructor/src/com/trial/constructor/Zoo.java
@@ -0,0 +1,28 @@
+package com.trial.constructor;
+
+public class Zoo {
+	
+	String name;
+	int noofanimals;
+	double zoosize;
+	byte noofzookeeper;
+	boolean iszooopeninsunday;
+	int zooentryfees;
+	
+	public Zoo() {
+		noofanimals=165;
+		zoosize=1069.758;
+		noofzookeeper=15;
+		zooentryfees=500;
+		iszooopeninsunday=false;
+		name="Vandalur";
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
---
+++ Constructor/src/com/trial/constructor/zootester.java
@@ -0,0 +1,16 @@
+package com.trial.constructor;
+
+public class zootester {
+	public static void main(String[] args) {
+		Zoo vandalur = new Zoo();
+		System.out.println(vandalur.name);
+		System.out.println(vandalur.noofanimals);
+		System.out.println(vandalur.zoosize + "sq.km");
+		System.out.println(vandalur.zooentryfees);
+		System.out.println(vandalur.iszooopeninsunday);
+		
+		
+	}
+	
+
+}
