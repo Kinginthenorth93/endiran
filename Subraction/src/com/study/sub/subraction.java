488357308caf94af8b024bd8d531b688240ab4d0
welcome <welcome@Endiran>
2022-02-24 17:22:02 +0530

    Subraction program

---
+++ Subraction/src/com/study/sub/subraction.java
@@ -0,0 +1,12 @@
+package com.study.sub;
+
+public class subraction {
+	public static void main(String[] args) {
+		int a = 200;
+		int b = 300;
+		int z = b-a;
+		System.out.println("Subraction of b and a= "+z);
+						
+	}
+
+}
