3d1c93977339da1c301a01d97165cc968d227f56
welcome <welcome@Endiran>
2022-02-24 17:18:29 +0530

    Multiplication

---
+++ Multiplication/src/com/maths/multiple/Multiplication.java
@@ -0,0 +1,16 @@
+package com.maths.multiple;
+
+public class Multiplication {
+	public static void main(String[] args) {
+		double sum1= 23.63;
+		double sum2= 63.25;
+		
+		double differnce= sum1 * sum2;
+		System.out.println("Difference= " + differnce);
+		
+		double multiply= sum1/sum2;
+		System.out.println("Multiply = " + multiply);
+				
+	}
+
+}
