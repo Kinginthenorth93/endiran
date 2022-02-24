0ccd857c158285d3fcad1a92e5cb9c326f636864
welcome <welcome@Endiran>
2022-02-24 17:23:36 +0530

    Unary operator

---
+++ Unary/src/com/study/operator/Logical.java
@@ -0,0 +1,13 @@
+package com.study.operator;
+
+public class Logical {
+	public static void main(String[] args) {
+		int a= 200;
+		int b= 400;
+		
+		System.out.println(a>b||b>a);
+		System.out.println(a==b||a!=b);
+		System.out.println(a==b&&a>b);
+	}
+
+}
---
+++ Unary/src/com/study/operator/Unary.java
@@ -0,0 +1,13 @@
+package com.study.operator;
+
+public class Unary {
+	public static void main(String[] args) {
+		int a = 10;
+		
+		System.out.println("PRE INCREMENT IS "+ ++a);
+		System.out.println("POST INCREMENT IS "+ a++);
+		System.out.println("PRE DECREMENT IS "+ --a);
+		System.out.println("POST DECREMENT IS "+ a--);
+	}
+
+}
