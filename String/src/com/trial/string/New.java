6ce10ceada186fc8d196697222e8f332d4c0f04e
welcome <welcome@Endiran>
2022-03-13 15:09:29 +0530

    String

---
+++ String/src/com/trial/string/New.java
@@ -0,0 +1,20 @@
+package com.trial.string;
+
+public class New {
+	public static void main(String[] args) {
+		String raa=new String("Doctor");
+		StringBuffer ra=new StringBuffer("doctor");
+		raa.concat("Surgeon");
+		ra.append(" mahi");
+		
+		System.out.println(raa);
+		System.out.println(ra);
+		System.out.println(ra.equals(raa));
+		System.out.println(raa.equals(ra));
+		System.out.println(ra.hashCode());
+		System.out.println(raa.endsWith(raa));
+	
+		
+	}
+
+}
